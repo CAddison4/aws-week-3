55276007f6eff58aae50a68bd1114cb49bd1a36b
Catherine Addison <caddison4@my.bcit.ca>
2023-01-27 13:09:13 -0800

    updated mysql variable names

--- database.js
+++ database.js
@@ -4,10 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 const pool = mysql
 	.createPool({
-		host: process.env.MYSQL_HOST,
-		user: process.env.MYSQL_USER,
-		password: process.env.MYSQL_PASSWORD,
-		database: process.env.MYSQL_DATABASE,
+		host: process.env.MYSQLHOST,
+		user: process.env.MYSQUSER,
+		password: process.env.MYSQLPASSWORD,
+		database: process.env.MYSQLDATABASE,
+		port: process.env.MYSQLPORT || 3306,
 	})
 	.promise();
 
