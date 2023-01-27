ae57ca1941439f686f2e1495794d7babdc8305bb
Catherine Addison <caddison4@my.bcit.ca>
2023-01-27 14:11:25 -0800

    changed port

--- database.js
+++ database.js
@@ -8,7 +8,7 @@ const pool = mysql
 		user: process.env.MYSQL_USER,
 		password: process.env.MYSQL_PASSWORD,
 		database: process.env.MYSQL_DATABASE,
-		port: process.env.MYSQL_PORT || 3306,
+		port: process.env.MYSQLPORT || 3306,
 	})
 	.promise();
 
